f1eb773cb5347f8693c662ba291b45c2a3398357
Clayton Carlson <claytonrc97@gmail.com>
2023-02-04 14:46:44 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const victory = record.find(recordFind => recordFind.result === 'W');
+    return victory ? victory.year : undefined;
+}
